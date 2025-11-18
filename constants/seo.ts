83c3246999c8d320ab248c71e70ea9be4cba27ef
Kejsan Coku <kejsancoku@gmail.com>
2025-11-18 11:26:40 +0100

    Update default base URL

--- constants/seo.ts
+++ constants/seo.ts
@@ -1,6 +1,6 @@
 const { VITE_SITE_URL } = import.meta.env;
 
-const DEFAULT_BASE_URL = 'https://makina-elektrike.netlify.app';
+const DEFAULT_BASE_URL = 'https://makinaelektrike.com';
 
 export const BASE_URL = (VITE_SITE_URL || DEFAULT_BASE_URL).replace(/\/+$/, '');
 export const DEFAULT_OG_IMAGE = 'https://raw.githubusercontent.com/Kejsan/makinaelektrike-aistudio/refs/heads/main/assets/BYD%20SEAL.jpg';
